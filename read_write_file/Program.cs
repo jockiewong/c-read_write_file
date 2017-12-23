5d173c0924b50fee0d2906f2a8a163a0b8a1c750
jockiewong <1301382381@qq.com>
2017-12-23 21:03:23 +0800

    1

---
+++ read_write_file/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace read_write_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = File.ReadAllText(@"D:\install.ini");
+            File.WriteAllText(@"d:\1.txt", str);           
+        }
+    }
+}
